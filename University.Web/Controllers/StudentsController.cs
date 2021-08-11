db2066ac5e449ec4c21c1bbe74e9c8b0ce47264b
David Santafe <santafedavid01@gmail.com>
2021-08-10 21:27:19 -0500

    CLASE 2021-08-10

--- University.Web/Controllers/StudentsController.cs
+++ University.Web/Controllers/StudentsController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
 using University.Web.Models;
 
 namespace University.Web.Controllers
@@ -9,9 +11,18 @@ namespace University.Web.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            return View(); //Vista igual que la acción
-            //return View("Index"); //Vista por nombre
-            //return View("Account/Login"); //Vista por carpeta/nombre
+            var students = new List<Student>(); //tamaño no fijo
+            students.Add(new Student { ID = 1, FirstMidName = "David", LastName = "Santafe", EnrollmentDate = DateTime.Now });
+            students.Add(new Student { ID = 2, FirstMidName = "Carson", LastName = "Alexander", EnrollmentDate = DateTime.Now });
+            students.Add(new Student { ID = 3, FirstMidName = "Meredith", LastName = "Alonso", EnrollmentDate = DateTime.Now });
+
+            ViewBag.Data = "Data de prueba";
+            ViewBag.Message = "Mensaje de prueba";
+
+            //ViewData["Data"] = "Data de prueba";
+            //ViewData["Message"] = "Mensaje de prueba";
+
+            return View(students); 
         }
 
         // Students/Create
@@ -24,12 +35,25 @@ namespace University.Web.Controllers
         [HttpPost]
         public ActionResult Create(Student student)
         {
-            var id = student.ID;
-            var firstMidName = student.FirstMidName;
-            var lastName = student.LastName;
-            var enrollmentDate = student.EnrollmentDate;
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View(student);
 
-            return View();
+                if (student.EnrollmentDate > DateTime.Now)
+                    throw new Exception("La fecha de matricula no puede ser mayor a la fecha actual");
+
+                var id = student.ID;
+                var firstMidName = student.FirstMidName;
+                var lastName = student.LastName;
+                var enrollmentDate = student.EnrollmentDate;
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+
+            return View(student);
         }
     }
 }
